01f0b3e981757abfc08a1646975bb89cde74f996
Diego Oliveira de Araujo <diego.dearaujo@ambevtech.com.br>
2022-03-24 09:02:03 -0300

    Input

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,30 @@
+import {
+  FormControl,
+  FormLabel,
+  InputProps as ChakraInputProps,
+  Input as ChakraInput,
+} from '@chakra-ui/react';
+
+interface InputProps extends ChakraInputProps {
+  name: string;
+  label?: string;
+}
+
+export function Input({ name, label, ...rest }: InputProps) {
+  return (
+    <FormControl>
+      {!!label && <FormLabel htmlFor="password">{label}</FormLabel>}
+
+      <ChakraInput
+        name={name}
+        id={name}
+        focusBorderColor="pink.500"
+        bgColor="gray.900"
+        variant="filled"
+        _hover={{ bgColor: 'gray.900' }}
+        size="lg"
+        {...rest}
+      />
+    </FormControl>
+  );
+}
